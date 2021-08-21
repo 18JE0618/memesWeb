f5faa4d9f5adf230be5d285ff508f8d107a395f4
18JE0618 <prashant01072000@gmail.com>
2021-08-21 22:26:01 +0530

    client added

---
+++ client/src/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect, createContext, useReducer, useContext } from "react";
+import Navbar from "./components/navbar";
+import "./App.css";
+import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom';
+import Home from './components/screens/Home';
+import Profile from './components/screens/Profile';
+import Signin from './components/screens/Signin';
+import Signup from './components/screens/Signup';
+import CreatePost from './components/screens/CreatePost';
+import { reducer, initialState } from "./reducers/userReducer";
+
+export const UserContext = createContext();
+
+const Routing = () => {
+  const history = useHistory();
+  const { state, dispatch } = useContext(UserContext);
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      dispatch({ type: "USER", payload: user }) // if user is logged in but he closes the application , so when he reopen the the application he must get access to protected resource for that this line of code will update the state with stored user details in local storage
+      history.push("/");
+    }
+    else {
+      history.push("/signin");
+    }
+  }, [])
+
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/signin">
+        <Signin />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
+      <Route path="/profile">
+        <Profile />
+      </Route>
+      <Route path="/createpost">
+        <CreatePost />
+      </Route>
+    </Switch>
+  )
+} //nice to meet
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <UserContext.Provider value={{ state, dispatch }}>
+      <BrowserRouter>
+        <Navbar />
+        <Routing />
+      </BrowserRouter>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
