531e79d11e42621578379b6e38d577e9a267fb37
18JE0618 <50778193+18JE0618@users.noreply.github.com>
2021-08-22 00:31:51 +0530

    Delete key

--- server/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGOURI: "mongodb+srv://prashant:WZs9WFRBM0HiJiNG@cluster0.un230.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET: "anyRandomString"
-}
